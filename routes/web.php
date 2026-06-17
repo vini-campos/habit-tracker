4cc175c4b0276b133a885a72931342a45d383e23
vini-campos <viniciuscampos131209@gmail.com>
2026-06-17 20:36:17 -0300

    feat: Início do projeto

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [SiteController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
