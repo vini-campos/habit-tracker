5b4e6c17ad22d089c4624b7f08b96ae20a60a79d
vini-campos <viniciuscampos131209@gmail.com>
2026-06-20 13:39:21 -0300

    feat(Auth): Implement login functionality with LoginController and login view

---
+++ resources/views/login.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <main class="py-10">
+        <h1>
+            Faça login
+        </h1>
+    </main>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/welcome', function () {
-    return view('welcome');
-});
-
+// SITE
 Route::get('/', [SiteController::class, 'index']);
+
+// LOGIN
+Route::get('/login', [LoginController::class, 'index']);
