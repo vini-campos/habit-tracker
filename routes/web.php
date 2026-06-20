b27abd0e0d94c0b11d97f2a0021b6a3486064b59
vini-campos <viniciuscampos131209@gmail.com>
2026-06-20 19:54:14 -0300

    feat(SiteController): Refactor index method to use compact for view data

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,19 @@ use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
 // SITE
-Route::get('/', [SiteController::class, 'index']);
+// name e opcional, serve para nao ter que trocar de todas as funcoes e do html que utiliza a rota
+Route::get('/', [SiteController::class, 'index'])->name('site.index');
 
 // LOGIN
-Route::get('/login', [LoginController::class, 'index']);
-Route::post('/login', [LoginController::class, 'authenticate']);
+Route::get('/login', [LoginController::class, 'index'])->name('site.login');
+Route::post('/login', [LoginController::class, 'authenticate'])->name('auth.login');
+
+// AUTH
+Route::middleware('auth')->group(function () {
+    // DASHBOARD
+    Route::get('/dashboard', [SiteController::class, 'dashboard'])->name('site.dashboard');
+
+    // LOGOUT
+    Route::post('/logout', [LoginController::class, 'logout'])->name('auth.logout');
+});
+
