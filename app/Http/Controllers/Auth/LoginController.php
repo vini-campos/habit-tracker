721272cc39f0dfe85258b34b2b6f2347821caafb
vini-campos <viniciuscampos131209@gmail.com>
2026-06-20 14:55:58 -0300

    feat(Auth): Implement authentication logic in LoginController

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,10 +4,28 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     public function index() {
         return view('login');
+    }
+
+    public function authenticate(Request $request) {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('/');
+        } else {
+            return back()->withErrors([
+                'email' => 'Credenciais inválidas.',
+            ]);
+        }
     }
 }
