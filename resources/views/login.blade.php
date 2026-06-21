47925d9e34f22825d8f61b582d612fcbc064011c
vini-campos <viniciuscampos131209@gmail.com>
2026-06-20 22:48:58 -0300

    feat(Login): Enhance login form layout and error message styling for better user experience

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,34 +1,56 @@
 <x-layout>
     <main class="py-10">
-        <h1>
-            Faça login
-        </h1>
+        <section class="bg-white max-w-150 mx-auto p-10 border-2 mt-4">
 
-        <section class="mt-4">
-            <form action="/login" method="post">
+            <h1 class="font-bold text-3xl mb-4">
+                Fazer login
+            </h1>
+
+            <p class="mb-4">
+                Insira seus dados para acessar
+            </p>
+
+            <form action="/login" method="post" class="flex flex-col">
                 <!-- Middleware que valida o form (obrigatorio) -->
                 @csrf
 
-                @error('email')
-                    <p class="text-red-500 text-xl mt-1">
-                        {{ $message }}
-                    </p>
-                @enderror
+                <div class="flex flex-col gap-2 mb-2">
+                    <label for="email">
+                        Email
+                    </label>
 
-                <input
-                    type="email" 
-                    name="email" 
-                    placeholder="Email" 
-                    class="bg-white p-2 border-2"
-                >
+                    <input
+                        type="email" 
+                        name="email" 
+                        placeholder="Email" 
+                        class="bg-white p-2 border-2 @error('email') border-red-500 @enderror"
+                    >
 
-                <input
-                    type="password" 
-                    name="password" 
-                    placeholder="**********" 
-                    class="bg-white p-2 border-2"
-                >
+                    @error('email')
+                        <p class="text-red-500 text-sm">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+                
+                <div class="flex flex-col gap-2 mb-4">
+                    <label for="password">
+                        Senha
+                    </label>
 
+                    <input
+                        type="password" 
+                        name="password" 
+                        placeholder="**********" 
+                        class="bg-white p-2 border-2 @error('password') border-red-500 @enderror"
+                    >
+
+                    @error('password')
+                        <p class="text-red-500 text-sm">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
                 <button type="submit" class="bg-white border-2 p-2">
                     Entrar
                 </button>
