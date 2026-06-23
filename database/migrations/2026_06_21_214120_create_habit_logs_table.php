9ecb5365faa7e9814b44b81e0ede7f2f4fa54ed8
vini-campos <viniciuscampos131209@gmail.com>
2026-06-23 18:59:08 -0300

    feat(Migration): Ensure unique constraint on habit_id and completed_at in habit_logs table

--- database/migrations/2026_06_21_214120_create_habit_logs_table.php
+++ database/migrations/2026_06_21_214120_create_habit_logs_table.php
@@ -25,6 +25,8 @@ return new class extends Migration
                 ->cascadeOnDelete();
             $table->date('completed_at');
             $table->timestamps();
+
+            $table->unique(['habit_id', 'completed_at']);
         });
     }
 
