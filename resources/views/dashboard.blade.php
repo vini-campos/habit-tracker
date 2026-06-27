d9815adc7676bfbd2e2061e6938bf86176f66f4d
vini-campos <viniciuscampos131209@gmail.com>
2026-06-27 10:00:09 -0300

    feat(Dashboard): Enhance user greeting and habit listing display

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,10 +4,37 @@
             Dashboard
         </h1>
 
-        @auth
-            <p>     
-                Bem vindo(a), {{ auth()->user()->name }}! 
-            </p>
-        @endauth
+
+        <p>     
+            Bem vindo(a), {{ auth()->user()->name }}! 
+        </p>
+
+        <div>
+            <h2 class="text-xl mt-4">
+                Listagem dos hábitos
+            </h2>
+
+            <ul class="flex flex-col gap-2">
+                @forelse ($habits as $item)
+                    <li class="pl-4">
+                        <div class="flex gap-2 items-center">
+                            <p class="font-bold text-xl">
+                                - {{ $item->name }}
+                            </p>
+                            <p>
+                                [{{ $item->habitLog->count() }}]
+                            </p>
+                        </div>
+                    </li>
+                    @empty
+                        <p>
+                            Ainda não há nenhum hábito cadastrado
+                        </p>
+                        <a href="/habito/cadastrar" class="bg-white p-2 border-2">
+                            Adicione um novo hábito agora
+                        </a>  
+                @endforelse
+            </ul>
+        </div>
     </main>
 </x-layout>
