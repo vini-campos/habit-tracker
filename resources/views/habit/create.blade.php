18674eccbea2ef9d6d30ae6f3f4ac9cd381f85e7
vini-campos <viniciuscampos131209@gmail.com>
2026-06-28 10:57:47 -0300

    feat(CreateHabitView): Add create habit form with validation error handling

---
+++ resources/views/habit/create.blade.php
@@ -0,0 +1,34 @@
+<x-layout>
+    <main class="py-10">
+        <h1>
+            cadastrar novo hábito
+        </h1>
+
+        <section class="bg-white max-w-150 mx-auto p-10 pb-6 border-2 mt-4">
+
+            <form action="{{ route('habit.store') }}" method="post" class="flex flex-col">
+                @csrf
+
+                <div class="flex flex-col gap-2 mb-2">
+                    <label for="name">
+                        Nome do hábito
+                    </label>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="nome do seu hábito"
+                        class="bg-white p-2 border-2 @error('name') border-red-500 @enderror"
+                    >
+                    @error('name')
+                        <p class="text-red-500 text-sm">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+                <button type="submit" class="bg-white border-2 p-2">
+                    Adicionar hábito
+                </button>
+            </form>
+        </section>
+    </main>
+</x-layout>
