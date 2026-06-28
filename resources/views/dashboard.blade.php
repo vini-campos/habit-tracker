cfca6db2562c3ca1996e2976648d36c9c184feed
vini-campos <viniciuscampos131209@gmail.com>
2026-06-28 10:58:27 -0300

    feat(Dashboard): Improve dashboard layout and add success message display

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,13 +1,20 @@
 <x-layout>
     <main class="py-10">
-        <h1>
+        <h1 class="font-bold text-4xl text-center ">
             Dashboard
         </h1>
 
+        <a href="{{ route('habit.create') }}" class="p-2 border-2 bg-white font-bold block">
+            Adicionar hábito
+        </a>
 
-        <p>     
-            Bem vindo(a), {{ auth()->user()->name }}! 
-        </p>
+        @session('success')
+            <div class="flex">
+                <p class="bg-green-100 border-2 border-green-400 text-green-700 p-3 rounded mb-4">
+                    {{ session('success') }}
+                </p>
+            </div>
+        @endsession
 
         <div>
             <h2 class="text-xl mt-4">
@@ -30,7 +37,7 @@
                         <p>
                             Ainda não há nenhum hábito cadastrado
                         </p>
-                        <a href="/habito/cadastrar" class="bg-white p-2 border-2">
+                        <a href="{{ route('habit.create') }}" class="bg-white p-2 border-2">
                             Adicione um novo hábito agora
                         </a>  
                 @endforelse
