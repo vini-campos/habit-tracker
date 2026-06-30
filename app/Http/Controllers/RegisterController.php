40d965d4af67a93791883fcbed3683d06761c21e
vini-campos <viniciuscampos131209@gmail.com>
2026-06-30 20:39:44 -0300

    fix(RegisterController): Update redirect route after user registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -22,6 +22,6 @@ class RegisterController extends Controller
         ]);
 
         Auth::login($user);
-        return redirect()->route('site.dashboard');
+        return redirect()->route('habits.index');
     }
 }
