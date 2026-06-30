5d32dba202cfe1eb5410c106f129eddfa679fc07
vini-campos <viniciuscampos131209@gmail.com>
2026-06-30 20:40:40 -0300

    refactor(routes): Simplify habit routes using resource controller and remove unused dashboard route

--- routes/web.php
+++ routes/web.php
@@ -18,17 +18,12 @@ Route::post('/login', [LoginController::class, 'authenticate'])->name('auth.logi
 Route::middleware('auth')->group(function ()
 {
     // DASHBOARD
-    Route::get('/dashboard', [SiteController::class, 'dashboard'])->name('site.dashboard');
 
     // LOGOUT
     Route::post('/logout', [LoginController::class, 'logout'])->name('auth.logout');
 
     // HABITS
-    Route::get('/dashboard/habits/create', [HabitController::class, 'create'])->name('habit.create'); 
-    Route::post('/dashboard/habits/create', [HabitController::class, 'store'])->name('habit.store');
-    Route::delete('/dashboard/habits/{habit}', [HabitController::class, 'destroy'])->name('habit.destroy');
-    Route::get('/dashboard/habits/{habit}/edit', [HabitController::class, 'edit'])->name('habit.edit');
-    Route::put('/dashboard/habits/{habit}', [HabitController::class, 'update'])->name('habit.update');
+    Route::resource('/dashboard/habits', HabitController::class)->except('show');
 });
 
 Route::get('/cadastro', [RegisterController::class, 'index'])->name('site.register');
