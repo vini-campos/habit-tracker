48b83daac36062e78a0bb1b0b82a4ebc48db4549
vini-campos <viniciuscampos131209@gmail.com>
2026-07-02 09:50:04 -0300

    feat(Settings): Create settings view for managing user habits

--- resources/views/habits/settings.blade.php
+++ resources/views/habits/settings.blade.php
@@ -13,7 +13,7 @@
 
         <div>
             <h2 class="text-lg mt-8 mb-2">
-                {{ date('d-m-Y')  }}
+                Configurar Hábitos
             </h2>
 
             <ul class="flex flex-col gap-2">
@@ -21,9 +21,22 @@
                     <li class="habit-shadow-lg p-2 bg-[#FFDAAC]">
                         <div class="flex gap-2 items-center">
                             <input type="checkbox" class="w-5 h-5 {{ $item->is_completed ? 'checked' : '' }} disabled">
+
                             <p class="font-bold text-lg">
                                 {{ $item->name }}
                             </p>
+
+                            <a class="bg-white p-1 hover:opacity-50" href="{{ route('habits.edit', $item->id) }}">
+                                <x-icons.edit />
+                            </a>
+
+                            <form action="{{ route('habits.destroy', $item) }}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="bg-red-500 text-white p-1 hover:opacity-50 cursor-pointer">
+                                    <x-icons.trash />
+                                </button>
+                            </form>
                         </div>
                     </li>
                 @empty
