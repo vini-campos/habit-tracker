29e1c6db5815ae9a52f8f60e496cdbf1fff59611
vini-campos <viniciuscampos131209@gmail.com>
2026-07-04 17:36:52 -0300

    fix(Requests): Remove unused Override import from LoginRequest and RegisterRequest

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -4,7 +4,6 @@ namespace App\Http\Requests;
 
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
-use Override;
 
 class RegisterRequest extends FormRequest
 {
