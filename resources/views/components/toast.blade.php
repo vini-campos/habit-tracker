0f2e976e069e324527737b4da4d35f7069213942
vini-campos <viniciuscampos131209@gmail.com>
2026-07-03 23:28:02 -0300

    feat(Toast): Implement toast notifications with dynamic styling for success, error, and warning messages

---
+++ resources/views/components/toast.blade.php
@@ -0,0 +1,29 @@
+@php
+    $type = session()->has('success') ? 'success'
+        : (session()->has('error') ? 'error'
+        : 'warning');
+
+    $message = session($type);
+
+    $styles = [
+        'success' => 'bg-green-100 border-green-400 text-green-700',
+        'error' => 'bg-red-100 border-red-400 text-red-700',
+        'warning' => 'bg-yellow-100 border-yellow-400 text-yellow-700',
+    ];
+@endphp
+
+@if (session()->has('success') || session()->has('error') || session()->has('warning'))
+    <div id="toast" class="absolute top-25 right-20 border-2 p-3 mb-4 flex gap-2 items-center bg-green-100 border-green-400
+        text-green-700 {{ $styles[$type] }}">
+
+        {{-- ICON --}}
+        <x-dynamic-component :component="'icons.' . $type" class="mt-4" />
+
+        {{-- MESSAGE --}}
+        <div>
+            <p>
+                {{ $message }}
+            </p>
+        </div>
+    </div>
+@endif
