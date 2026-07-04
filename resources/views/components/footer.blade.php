9f4824819f73c86b3141e1ad727d82c77b542054
vini-campos <viniciuscampos131209@gmail.com>
2026-07-04 11:40:48 -0300

    feat(UI): Update footer and navbar styles for improved readability

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="bg-white border-t-2 p-4">
     <p class="text-center">
-        &copy; 2026 <a href="https://github.com/vini-campos" target="_blank" class="underline hover:text-habit-orange">Vinicius Campos</a>
-        Código fonte está no <a href="https://github.com/vini-campos/habit-tracker" class="underline hover:text-habit-orange">Github</a>
+        &copy;2026 <a href="https://github.com/vini-campos" target="_blank" class="font-bold underline hover:text-habit-orange">Vinicius Campos</a>
+        Código fonte no <a href="https://github.com/vini-campos/habit-tracker" class="font-bold underline hover:text-habit-orange">Github</a>
     </p>
 </footer>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -2,24 +2,24 @@
     <ul class="flex gap-4 items-center ">
         <li>
             <a href="{{ route('habits.index') }}" class="{{ Route::is('habits.index') ? 'font-bold underline' : '' }}
-                font-bold text-md border-r-2 border-habit-orange pr-2 hover:underline">
+                font-bold text-lg border-r-2 border-habit-orange pr-2 hover:underline">
                 Hoje
             </a> 
         </li>
         <li>
             <a href="{{ route('habits.history') }}" class="{{ Route::is('habits.history') ? 'font-bold underline' : '' }}
-                font-bold text-md border-r-2 border-habit-orange pr-2 hover:underline">
+                font-bold text-lg border-r-2 border-habit-orange pr-2 hover:underline">
                 Histórico
             </a> 
         </li>
         <li>
-            <a href="#" class="font-bold text-md border-r-2 border-habit-orange pr-2 hover:underline">
+            <a href="#" class="font-bold text-lg border-r-2 border-habit-orange pr-2 hover:underline cursor-not-allowed">
                 Calendário
             </a> 
         </li>
         <li>
             <a href="{{ route('habits.settings') }}" class="{{ Route::is('habits.settings') ? 'font-bold underline' : '' }}
-                font-bold text-md border-habit-orange hover:underline">
+                font-bold text-lg border-habit-orange hover:underline">
                 Gerenciar hábitos
             </a> 
         </li>
