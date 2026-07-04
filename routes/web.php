ad58ea031e74335cb3632d7f46096e471b7c3065
vini-campos <viniciuscampos131209@gmail.com>
2026-07-03 23:27:44 -0300

    fix(Habit): Correct history route to include optional year parameter

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,8 @@ Route::middleware('auth')->group(function ()
     Route::post('/logout', [LoginController::class, 'logout'])->name('auth.logout');
 
     // HABITS
+    Route::get('/dashboard/habits/history/{year?}', [HabitController::class, 'history'])->name('habits.history');
     Route::resource('/dashboard/habits', HabitController::class)->except('show');
-    Route::get('/dashboard/habits/history', [HabitController::class, 'history'])->name('habits.history');
     Route::get('/dashboard/habits/config', [HabitController::class, 'settings'])->name('habits.settings');
     Route::post('/dashboard/habits/{habit}/toggle', [HabitController::class, 'toggle'])->name('habits.toggle');
 });
