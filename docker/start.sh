1f6f93d830c9e05007325e9685a958bf571ea908
Vinicius Campos <viniciuscampos131209@gmail.com>
2026-09-06 12:40:26 -0300

    Update start.sh to create .env and clean caches
    Create .env file from system environment variables and clean caches before starting services.

--- docker/start.sh
+++ docker/start.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+echo "==> Criando .env a partir das env vars do sistema..."
+printenv > /var/www/html/.env
+
 echo "==> Limpando caches..."
 php artisan config:clear
 php artisan view:clear
 php artisan route:clear
+
+echo "==> Gerando APP_KEY..."
+php artisan key:generate --force
 
 echo "==> Rodando migrations..."
 php artisan migrate --force
